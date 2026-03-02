18e1a814f69c6a9515f2fd4fdaad3149b8663c03
0xLogN <nutdriver716@gmail.com>
2026-03-01 17:51:07 -0800

    Initial commit 2: electric boogaloo

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "zirco-auto-cloudflare-bl";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ rust-overlay.overlays.default ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (rust-bin.nightly.latest.default.override {
+              extensions = [ "rust-src" "rust-analyzer" ];
+            })
+
+            nixfmt
+          ];
+        };
+      }
+    );
+}
